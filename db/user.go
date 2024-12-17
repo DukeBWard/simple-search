a7ec26bdafdab8fda35303e4eb91616d09f9d0bf
Luke Ward <wardlukeb@gmail.com>
2024-12-17 15:10:24 -0500

    adding some basic functionality"

--- db/user.go
+++ db/user.go
@@ -1,6 +1,11 @@
 package db
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID        string     `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
@@ -17,4 +22,29 @@ func (u *User) CreateAdmin() error {
 		Password: "test password",
 		IsAdmin:  true,
 	}
+	// hashin
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return errors.New("error with password")
+	}
+	user.Password = string(password)
+	// put user in DB
+	if err := DBConnect.Create(&user).Error; err != nil {
+		return errors.New("error creating user")
+	}
+	return nil
+}
+
+func (u *User) LoginAsAdmin(email string, password string) (*User, error) {
+	// find the user
+	// question marks are the placeholders in GORM
+	if err := DBConnect.Where("email = ? AND is_admin = ?", email, true).First(&u).Error; err != nil {
+		return nil, errors.New("user not found")
+	}
+	// should have user now
+	// compare password
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
+		return nil, errors.New("invalid password")
+	}
+	return u, nil
 }
--- views/login_templ.go
+++ views/login_templ.go
@@ -8,6 +8,7 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+// the hx tags are used for form submit through htmx
 func Login() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
