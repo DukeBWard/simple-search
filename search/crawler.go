a804200a59193d56e2ff721d8f9309c9586a3c2c
Luke Ward <wardlukeb@gmail.com>
2024-12-18 15:12:27 -0500

    going to the gytm

--- search/crawler.go
+++ search/crawler.go
@@ -1,6 +1,12 @@
 package search
 
-import "time"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
 
 type CrawlData struct {
 	Url          string `json:"url"`
@@ -20,4 +26,25 @@ type ParsedBody struct {
 type Links struct {
 	Internal []string
 	External []string
+}
+
+func runCrawl(inputUrl string) CrawlData {
+	resp, err := http.Get(inputUrl)
+	baseUrl, err := url.Parse(inputUrl)
+	// error or empty
+	if err != nil || resp == nil {
+		fmt.Println("couldnt crawl this body")
+		return CrawlData{Url: inputUrl, Success: false, ResponseCode: 0, CrawlData: ParsedBody{}}
+	}
+
+	// defer to the end of the call
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("error not 200")
+		return CrawlData{Url: inputUrl, Success: false, ResponseCode: resp.StatusCode, CrawlData: ParsedBody{}}
+	}
+	content := resp.Header.Get("Content-Type")
+	if strings.HasPrefix(co)
+
 }
